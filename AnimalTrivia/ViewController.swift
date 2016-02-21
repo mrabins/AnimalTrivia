ed8ffb4d2a400eeb06b8ff53a682d7fcc4a3c180
Mark Rabins <markrabins@gmail.com>
2016-02-20 23:05:06 -0800

    Publish of assets

--- AnimalTrivia/ViewController.swift
+++ AnimalTrivia/ViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet weak var aLabel: UILabel!
+    @IBOutlet weak var bLabel: UILabel!
+    @IBOutlet weak var cLabel: UILabel!
+    
+    @IBOutlet weak var aButton: UIButton!
+    @IBOutlet weak var bButton: UIButton!
+    @IBOutlet weak var cButton: UIButton!
+    
+    @IBOutlet weak var incorrectAImageView: UIImageView!
+    @IBOutlet weak var incorrectBImageView: UIImageView!
+    @IBOutlet weak var correctCImageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +34,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func aButtonPressed(sender: UIButton) {
+
+    }
+    
+    @IBAction func bButtonPressed(sender: UIButton) {
+
+    }
+    
+    @IBAction func cbuttonPressed(sender: UIButton) {
+
+    }
+    
+    @IBAction func nextButtonPressed(sender: UIButton) {
+        
+    }
+    
 
 }
 
